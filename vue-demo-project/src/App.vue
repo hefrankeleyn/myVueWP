a1138b7045632ebafd4ab3630bc70e3e81ef6228
Li Fei <hefrankeleyn@gamil.com>
2021-07-26 07:07:49 +0800

    一个vue项目demo

---
+++ vue-demo-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">hello</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // 注册引入进来的组件
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style lang="less">
+
+</style>
