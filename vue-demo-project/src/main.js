a1b6b45c20cc798c1e0ee5974aac270c51b3d851
Li Fei <hefrankeleyn@gamil.com>
2021-09-09 07:08:22 +0800

    动画补充

--- vue-demo-project/src/main.js
+++ vue-demo-project/src/main.js
@@ -1,5 +1,15 @@
 // 引入vue
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+new Vue({
+    // 渲染函数， 这个函数还是比较重要的
+    // render: h=>h(App),
+    render: createElement => createElement('h1',{}, 'hello'),
+}).$mount('#app');
+
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
